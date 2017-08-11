48ddd8eace5ceba49ac29e995043c278ac32359b
Devon Zara <hello@devonzara.com>
2017-08-11 14:53:16 -0400

    Add commonly used hooks

--- PluginStarterTemplate.cs
+++ PluginStarterTemplate.cs
@@ -14,5 +14,29 @@
         {
             Puts("[INFO] Plugin loaded!");
         }
+
+        private void Unload() {
+            Puts("[INFO] Plugin unloaded!");
+        }
+
+        private void OnPlayerInit(BasePlayer player)
+        {
+            Puts($"[INFO] Player {player.displayName} initialized!");
+        }
+
+        private void OnPlayerSleepEnded(BasePlayer player)
+        {
+            Puts($"[INFO] Player {player.displayName} has woken!");
+        }
+
+        private void OnPlayerSleep(BasePlayer player)
+        {
+            Puts($"[INFO] Player {player.displayName} has fallen asleep!");
+        }
+
+        private void OnPlayerDisconnected(BasePlayer player)
+        {
+            Puts($"[INFO] Player {player.displayName} has disconnected!");
+        }
     }
 }
