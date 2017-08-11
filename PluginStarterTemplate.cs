2497d62357b8e91ddc630a23f5cd904adf78c267
Devon Zara <hello@devonzara.com>
2017-08-11 14:36:24 -0400

    Initial commit

---
+++ PluginStarterTemplate.cs
@@ -0,0 +1,18 @@
+﻿namespace Oxide.Plugins
+{
+    [Info("PluginStarterTemplate", "AuthorName", "0.1.0", ResourceId = 0000)]
+    [Description("A basic plugin template with some commonly used hooks.")]
+
+    public class PluginStarterTemplate : RustPlugin
+    {
+        private void Init()
+        {
+            Puts("[INFO] Plugin initialized!");
+        }
+
+        private void Loaded()
+        {
+            Puts("[INFO] Plugin loaded!");
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,7 @@
+<p align="center"># Oxide-RustPlugins
+
+</p>
+
+## About
+
+A simple collection respoitory of custom plugins, boilerplates, starter templates, and code snippets for Rust.
